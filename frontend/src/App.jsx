1cba5adad132d853841f8b6a06d83b1433d00119
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2024-04-13 09:54:35 +0530

    Created the frontend part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div className="bg-blue-600 p-4 text-white flex justify-between">
+				hello
+			</div>
+		</>
+	);
+}
+
+export default App;
